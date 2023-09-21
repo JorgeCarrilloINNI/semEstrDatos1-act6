abbb219b63019759341cf1dd557e3304706d7d91
JorgeCarrilloINNI <142334309+JorgeCarrilloINNI@users.noreply.github.com>
2023-09-21 10:47:42 -0600

    avances

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Cola.h"
+
+using namespace std;
+
+int main(){
+	
+	Cola MiCola;
+    Constancia x;
+    
+    int opcion = 0;
+    
+    while(opcion != 3){
+    	cout<<"1- Dar de alta alumno (enqueue)"<<endl;
+    	cout<<"2- Elaborar constancia (dequeue)"<<endl;
+    	cout<<"3- Salir"<<endl;
+    	cout<<"Opcion: ";
+    	cin>>opcion;
+    	
+    	switch(opcion){
+    		case 1:
+    			cin>>x;
+    			MiCola.enqueue(x);
+    			break;
+    		case 2:
+    			cout<<MiCola.dequeue()<<endl;
+    			break;
+    		case 3:
+    			cout<<"Saliendo..."<<endl;
+    			break;
+    		default:
+    			cout<<"Seleccione una opcion correcta.."<<endl;
+    			break;
+		}
+	}
+	
+	return 0;
+}
